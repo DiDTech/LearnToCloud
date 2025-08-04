73dae8269028002663b4bb58fea2c143ff75568c
DiDTech <davidmbftech@gmail.com>
2025-08-04 16:10:58 +0100

    First commit

--- newGhRepo.sh
+++ newGhRepo.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+repoName=$1
 while [ -z "$repoName" ]
 do
-  echo "Provide a repository name"
-  read -r -p "Repository name: " repoName
+    echo 'Provide a repository name'
+    read -r -p 'Repository name: ' repoName
 done
+echo "# $repoName" >> README.md
+git init
+git add .
+git commit -m "First commit"
